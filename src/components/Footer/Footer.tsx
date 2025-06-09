47197e344fc79f7dac404cc78c01795d18e7a5c7
Jennifer <jennifernhf@gmail.com>
2025-06-08 17:15:52 -0700

    fix footer links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -13,10 +13,10 @@ export default function Footer() {
       <div className="footer-wrapper">
         <ul className="links">
           <li>
-            <Link className="link" to="/">Codepen</Link>
+            <Link className="link" to="https://codepen.io/sparkleOven">Codepen</Link>
           </li>
           <li>
-            <Link className="link" to="/">Github</Link>
+            <Link className="link" to="https://github.com/jenhedg">Github</Link>
           </li>
           <li>
             <Link className="link" to="mailto: jenhedg@gmail.com">Email</Link>
