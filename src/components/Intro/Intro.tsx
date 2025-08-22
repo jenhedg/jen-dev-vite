6a816fea35fbcc3dcaf24b661405ee6462a565a4
Jennifer <jennifernhf@gmail.com>
2025-08-21 19:15:57 -0700

    header flexbox fix, intro updates

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -13,10 +13,20 @@ export default function Intro() {
                     <h2 className="intro-title"><span>Jennifer Hedgcock</span></h2>
                     <div className="intro-content">
                         <p className="intro-subtitle"><span>Front End Developer</span></p>
-                        <p className="intro-text">I'm currently a Web Developer at <Link className="link" to="https://www.granicus.com" target="_blank">Granicus</Link> (*formerly <Link className="link" to="https://www.simpleviewinc.com/" target="_blank"> Simpleview</Link>)</p>
-                        <p className="intro-text"> I have 9 years of experience building scalable, user-focused web applications.<br/> I enjoy working with <strong>CMS</strong> and <strong>SaaS</strong> technologies, <strong>design implementation</strong>, <strong>UI frameworks</strong> and <strong>design systems</strong>, <strong>process optimization</strong>, <strong>documentation</strong>.
+                        <p className="intro-text">I'm currently a Web Developer at <Link className="link" to="https://www.granicus.com" target="_blank">Granicus</Link> (*formerly <Link className="link" to="https://www.simpleviewinc.com/" target="_blank"> Simpleview</Link>), build data-driven webapps for the travel and tourism industry.</p>
+
+                        <p className="intro-text">
+                            I enjoy working with the following:
+                            <ul className="intro-list">
+                                <li className="list-item"><strong>React</strong></li>
+                                <li className="list-item"><strong>CMS & SaaS Systems</strong></li>
+                                <li className="list-item"><strong>XD Implemetation</strong></li>
+                                <li className="list-item"><strong>UI Frameworks</strong></li>
+                                <li className="list-item"><strong>Technical Documentation</strong></li>
+                                <li className="list-item"><strong>Process Optimization</strong></li>
+                            </ul>
                         </p>
-                        <p className="intro-text">I built this site with <strong>React.js</strong>, <strong>TypeScript</strong>, <strong>Vite</strong> and <strong>Vercel</strong></p>
+                        <p className="intro-text">I built this site from scratch with <strong>React.js</strong>, <strong>TypeScript</strong>, <strong>Vite</strong> and <strong>Vercel.</strong></p>
                     </div>
                 </article>  
             </div>
